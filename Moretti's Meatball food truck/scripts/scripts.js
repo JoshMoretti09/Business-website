3492a56168d8b803d8e8bd590f1aaabe94cb30cb
Josh Moretti <jmoretti09@gmail.com>
2015-09-03 21:27:22 -0400

    Added structure and some bootstrap

---
+++ Moretti's Meatball food truck/scripts/scripts.js
@@ -0,0 +1,11 @@
+// $(document).ready(function () {
+ 	
+//  	 	//After 5 seconds shows modal
+//  		setTimeout(function (){
+//  			$(".popinModal").fadeIn()
+//  	}, 5000);
+
+//  		$(".exit").click(function (){
+//  	 	$(".popinModal").fadeOut();
+//  	 });
+//  	});
