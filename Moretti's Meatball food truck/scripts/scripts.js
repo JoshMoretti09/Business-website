5353f8c7db79f832c56689de356634beb8f7f440
Josh Moretti <jmoretti09@gmail.com>
2015-09-08 21:28:33 -0400

    Added a number of new items

--- Moretti's Meatball food truck/scripts/scripts.js
+++ Moretti's Meatball food truck/scripts/scripts.js
@@ -1,11 +1,22 @@
-// $(document).ready(function () {
+$(document).ready(function () {
  	
-//  	 	//After 5 seconds shows modal
-//  		setTimeout(function (){
-//  			$(".popinModal").fadeIn()
-//  	}, 5000);
+ 	 	//After 5 seconds shows modal
+ 		setTimeout(function (){
+ 			$(".popinModal").fadeIn()
+ 	}, 5000);
 
-//  		$(".exit").click(function (){
-//  	 	$(".popinModal").fadeOut();
-//  	 });
-//  	});
+ 		$(".exit").click(function (){
+ 	 	$(".popinModal").fadeOut();
+ 	 });
+ 	$('.navbar-wrapper').sticky({
+ 		topSpacing:0,
+  		getWidthFrom: '.navbar-wrapper',
+  		responsiveWidth: true,
+	});
+ 	$(".animsition").animsition({
+	  inClass: 'fade-in-up-lg',
+	  linkElement: 'header a',
+	  inDuration: 1000,
+	  outDuration: 1000
+	});
+});
